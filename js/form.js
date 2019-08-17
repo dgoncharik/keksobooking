171ebaf6523e8f5105945fc9ada4acf7cdcc9612
Гончарик Денис <dgoncharik27@gmail.com>
2019-08-18 00:01:09 +0300

    Возможность загружать фото в форму + отрисовка превью

--- js/form.js
+++ js/form.js
@@ -10,6 +10,12 @@
   var timeOutElement = adFormElement.querySelector('#timeout');
   var formElements = Array.from(adFormElement.querySelectorAll(['input', 'select', 'textarea', 'button', 'label']));
   var htmlClassDisabled = 'ad-form--disabled';
+  var inputAvatar = adFormElement.querySelector('#avatar');
+  var avatarContainer = adFormElement.querySelector('.ad-form-header__preview');
+  var previewAvatar = avatarContainer.querySelector('img');
+  var inputHouseImages = adFormElement.querySelector('#images');
+  var housingPreviewContainer = adFormElement.querySelector('.ad-form__photo-container');
+  var housingPhotos = [];
   var onAdFormElementSubmit = null;
   var onAdFormElementReset = null;
 
@@ -69,7 +75,125 @@
   }
 
   function getFormData() {
-    return new FormData(adFormElement);
+    var formData = new FormData(adFormElement);
+    housingPhotos.forEach(photo => {
+      formData.append('images', photo);
+    })
+    return formData;
+  }
+
+  function typeUploadFileIsImage(uploadedFile) {
+    return uploadedFile.type.toLowerCase().startsWith('image/');
+  }
+
+  function setDataUrl(imgElement, uploadedFile) {
+    var reader = new FileReader();
+    reader.addEventListener('load', function() {
+      imgElement.src = reader.result;
+    })
+    reader.readAsDataURL(uploadedFile);
+  }
+
+  function onAvatarContainerMouseEnter() {
+    avatarContainer.classList.add('ad-form-header__preview--delete');
+  }
+
+  function onAvatarContainerMouseLeave() {
+    avatarContainer.classList.remove('ad-form-header__preview--delete');
+  }
+
+  function onAvatarContainerClick() {
+    onAvatarContainerMouseLeave();
+    setDefaultAvatar();
+  }
+
+  function setAvatar(file) {
+    if (typeUploadFileIsImage(file)) {
+      setDataUrl(previewAvatar, file);
+      avatarContainer.addEventListener('mouseenter', onAvatarContainerMouseEnter);
+      avatarContainer.addEventListener('mouseleave', onAvatarContainerMouseLeave);
+      avatarContainer.addEventListener('click', onAvatarContainerClick);
+    } else {
+      setDefaultAvatar();
+    }
+  }
+
+  function setDefaultAvatar() {
+    avatarContainer.removeEventListener('mouseenter', onAvatarContainerMouseEnter);
+    avatarContainer.removeEventListener('mouseleave', onAvatarContainerMouseLeave);
+    avatarContainer.removeEventListener('click', onAvatarContainerClick);
+    previewAvatar.src = 'img/muffin-grey.svg';
+    inputAvatar.value = '';
+  }
+
+  function removeEmptyPreviews() {
+    var photos = housingPreviewContainer.querySelectorAll('.ad-form__photo');
+    photos.forEach(photo => {
+      if (!photo.querySelector('img')) {
+        photo.remove();
+      }
+    })
+  }
+
+  function restoreEmptyPreview() {
+    var btnUpload = housingPreviewContainer.querySelector('.ad-form__upload');
+    if (housingPreviewContainer.lastElementChild === btnUpload) {
+      var emtyPreview = renderHousingPreview(false);
+      housingPreviewContainer.appendChild(emtyPreview);
+    }
+  }
+
+  function clearHousingPhotos() {
+    var photos = Array.from(housingPreviewContainer.querySelectorAll('.ad-form__photo'));
+    housingPhotos = [];
+    photos.forEach(photo => {
+      photo.remove();
+    })
+    restoreEmptyPreview();
+  }
+
+  function renderHousingPreview(file) {
+    var preview = document.createElement('div');
+    preview.classList.add('ad-form__photo');
+    if (file) {
+      preview.file = file;
+      var img = document.createElement('img');
+      img.width = 70;
+      img.height = 70;
+      img.style.borderRadius = '5px';
+      img.alt = 'Фотография жилья';
+      setDataUrl(img, file);
+      preview.addEventListener('click', function() {
+        var indexFile = housingPhotos.indexOf(preview.file);
+        if (indexFile != -1) {
+          housingPhotos.splice(indexFile, 1);
+          preview.remove();
+          restoreEmptyPreview();
+        }
+      })
+      preview.addEventListener('mouseenter', function() {
+        preview.classList.add('ad-form__photo--delete')
+      })
+      preview.addEventListener('mouseleave', function() {
+        preview.classList.remove('ad-form__photo--delete')
+      })
+      preview.appendChild(img);
+    }
+    return preview;
+  }
+
+  function addHousingPhotos(arrFiles) {
+    var fragment = document.createDocumentFragment();
+    removeEmptyPreviews();
+    arrFiles.forEach(file => {
+      if (typeUploadFileIsImage(file)) {
+        housingPhotos.push(file);
+        fragment.appendChild(renderHousingPreview(file));
+      }
+    })
+    housingPreviewContainer.appendChild(fragment);
+    restoreEmptyPreview();
+    inputHouseImages.value = '';
   }
 
   function onHousTypeElementClick(evt) {
@@ -84,19 +208,27 @@
     setValueField(timeInElement, timeOutElement.value);
   }
 
-  housTypeElement.addEventListener('change', function(evt) {
-    onHousTypeElementClick(evt);
-  });
+  housTypeElement.addEventListener('change', onHousTypeElementClick);
 
-  timeInElement.addEventListener('change', function() {
-    onTimeInElementChange();
-  });
+  timeInElement.addEventListener('change', onTimeInElementChange);
 
-  timeOutElement.addEventListener('change', function() {
-    onTimeOutElementChange();
-  });
+  timeOutElement.addEventListener('change', onTimeOutElementChange);
+
+  inputAvatar.addEventListener('change', function(evt) {
+    evt.preventDefault();
+    var uploadedFile = inputAvatar.files[0];
+    setAvatar(uploadedFile);
+  })
+
+  inputHouseImages.addEventListener('change', function(evt) {
+    evt.preventDefault();
+    var files = Array.from(inputHouseImages.files);
+    addHousingPhotos(files);
+  })
 
   adFormElement.addEventListener('reset', function() {
+    setDefaultAvatar();
+    clearHousingPhotos();
     if (onAdFormElementReset) {
       setTimeout(onAdFormElementReset, 0);
     }
@@ -107,6 +239,12 @@
       onAdFormElementSubmit(evt);
     }
   })
+
+  function initialization() {
+    setMinPrice(housingTypeToPrice[housTypeElement.value]);
+  }
+
+  initialization();
 
   window.form = {
     reset: resetForm,
